eb362c797eef8f5cbc5b9c2d87ea1ff3f8467979
Wesley Cesar <72465364+wesley587@users.noreply.github.com>
2021-04-21 00:20:10 -0300

    Update new_aomic.py

--- new_aomic.py
+++ new_aomic.py
@@ -2,11 +2,13 @@ import re
 import argparse
 import requests
 import yaml
+import os
 
 arguments = argparse.ArgumentParser()
 arguments.add_argument('-t', action='store', dest='uuid', help='', required=True)
 arguments.add_argument('-testnumber', action='store', dest='testnumber', required=False)
 arguments.add_argument('-action', action='store', dest='action', required=False)
+
 parse = arguments.parse_args()
 
 if re.match(r'T\d*$|T\d*.\d*$', parse.uuid):
@@ -34,9 +36,12 @@ if re.match(r'T\d*$|T\d*.\d*$', parse.uuid):
 
         def execute(self):
             content = yaml.safe_load(self.content)
-            print(content['atomic_tests'][int(self.testnumber) - 1])
+            command = content['atomic_tests'][int(self.testnumber) -1]['executor']['command'].split('\n')
+            shell = content['atomic_tests'][int(self.testnumber) -1]['executor']['name']
+            [os.system(x) for x in command if x != '']
 
     start = atomic()
     start.main()
 else:
     print('Technique not found, try again')
+
