21dad35be8a45d063b14f7ccfa47220f13b8d147
Wesley Cesar <72465364+wesley587@users.noreply.github.com>
2021-08-04 14:52:06 -0300

    Update client.py

--- bin/client.py
+++ bin/client.py
@@ -10,6 +10,7 @@ from json import dumps
 import socket
 import sys
 import re
+from json import loads
 
 
 first_execution = False
@@ -58,96 +59,20 @@ if first_execution:
 import requests
 import yaml
 
-ip = '0.0.0.0'
-port = 20000
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((ip, port))
-buffer = client_socket.recv(1028).decode()
-print(buffer)
-
-parser = buffer.replace('-', '.-').split('.')
-
-
 
 class atomic:
-    def __init__(self):
-        self.control = self.generate_control(buffer)
+    def __init__(self, buffer):
+        self.control = loads(buffer)
         print(self.control)
         self.except_time = self.control['except_time']
-    
-    def generate_control(self, buffer):
-        args = {'date': datetime.now().strftime('%d-%m-%Y -%H-%M-%S'),'except_time': 120}
-        for x in parser:
-            if '-tn' in x:
-                x = x.replace('-tn', 'testnumber')
-                print(x)
-                args['action'] = 'execute'
-            else:
-                x = self.check_command_name(x)
-            if x:
-                if len(x.split()) > 1:
-                    x = x.split() 
-                    args[x[0]] = x[1]
-                else:
-                    args['action'] = x
-        return args
-    
-    def check_command_name(self, arg):
-        commands = {'uuid': ['-t', '--testnumber', '-T'],
-                'testnuber': ['-tn', '--testnumber'],
-                'except_time': ['-ex', '--except_time'],
-                'cleanup': ['-c', '--cleanup'],
-                'showdetailsbrief': ['-sdb', '--showdetailsbrief'],
-                'showdetails': ['-sd', '--showdetails'],
-                'getprereqs': ['-gp', '--getprereqs']}
-        for k, v in commands.items():
-            for c in v:
-                if c in arg:
-                    return arg.replace(c, k)
-    
-    def validate_args(self, parse):
-        a = parse.showdetailsbrief
-        b = parse.showdetails
-        c = parse.cleanup
-        d = parse.testnumber
-        e = parse.getprereqs
-        if a and b or a and c or a and d or a and e or b and c or b and d or b and e or c and e:
-            print(f'[Error] INVALID ARGS')
-            exit(1)
-        
-    
+     
     def cache(self):
         data = dumps(self.control)
         with open(f'cache/{self.control["date"]}.json', 'w') as file:
             print(f'[*] Salving cache')
             file.write(data)
             print(f'[Info] Cache salved')
-
-    
-    def generate_dict(self, parse):
-        values_dict = dict()
-        values_dict['action'] = self.mode(parse)
-        values_dict['uuid'] = args['-t']
-        values_dict['testnumber'] = parse.testnumber
-        values_dict['date'] = datetime.now().strftime('%d-%m-%y %H-%M-%S')
-        return values_dict
         
-    def mode(self, parse):
-        for k, v in parse.items():
-            if parse.cleanup:
-                return 'cleanup'
-            elif parse.showdetails:
-                return 'showdetails'
-            elif parse.showdetailsbrief:
-                return 'showdetailsbrief'
-            elif parse.getprereqs:
-                return 'getprereqs'
-            elif parse.testnumber:
-                return 'execute'
-        else:
-            print('[Error] ERROR, PASS AN ARGUMENT')
-            exit(0)
 
     def main(self):
         self.requests()
@@ -181,6 +106,7 @@ class atomic:
         else:
             print(f'[Info] {self.control["uuid"]} is invalid')
             print('[Error] Technique not found, try again....')
+            client_socket.send('[Error] Technique not found, try again....').encode()
             exit(0)
 
     def execute(self):
@@ -194,7 +120,7 @@ class atomic:
 
         shell = content['atomic_tests'][int(self.control["testnumber"]) - 1]['executor']['name']
         print(f'[Info] Runining on: {shell}')
-        print(f'\n          ---------- OUTPUT ----------\n')
+        client_output = '\n          ---------- OUTPUT ----------\n'
 
         output = list()
         for x in command:
@@ -216,22 +142,26 @@ class atomic:
                         x = self.input_arguments(x).split()
                         output.append(subprocess.check_output([self.input_arguments(x)], shell=True))
 
-
-        [print(x.decode('windows-1252')) for x in output]
-
+        [client_socket.send((client_output + x.decode('windows-1252')).encode()) for x in output]
+        
 
     def parsing(self):
         if self.control['action'].lower() == 'getprereqs':
             self.getprereqs()
 
         elif self.control['action'].lower() == 'showdetails':
-            print(f'\n     ---------- DETAILS ----------\n')
+            client_output = f'\n     ---------- DETAILS ----------\n'
             print(self.control['content'])
+            client_socket.send((client_output + self.control['content']).encode())
 
         elif self.control['action'].lower() == 'showdetailsbrief':
-            print(f'\n     ---------- DETAILS BRIEF ----------\n')
-            self.showdetailsbrief()
-
+            client_output = f'\n     ---------- DETAILS BRIEF ----------\n'
+            dt_brief = self.showdetailsbrief()
+            print(dt_brief)
+            for x in self.showdetailsbrief():
+                client_output += '\n' + x
+            client_socket.send(client_output.encode())
+            
     def getprereqs(self):
         print(f"[*] Instaling depedencies")
         content = yaml.safe_load(self.control['content'])
@@ -318,9 +248,20 @@ class atomic:
 
     def showdetailsbrief(self):
         yaml_contet = yaml.safe_load(self.control['content'])['atomic_tests']
-        [print(f'[{c + 1}] {yaml_contet[c]["name"]}') for c in range(0, len(yaml_contet)) if
+        dt_brief = [f'[{c + 1}] {yaml_contet[c]["name"]}' for c in range(0, len(yaml_contet)) if
             system.split('-')[0] in yaml_contet[c]['supported_platforms']]
-        print('')
+        print(dt_brief)
+        return dt_brief
+
 if __name__ == '__main__':
-    start = atomic()
+    ip = '0.0.0.0'
+    port = 45000
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port))
+    buffer = client_socket.recv(1028).decode()
+    print(f'[INFO] We receiv: {buffer}')
+
+    parser = buffer.replace('-', '.-').split('.')
+    start = atomic(buffer)
     start.main()
